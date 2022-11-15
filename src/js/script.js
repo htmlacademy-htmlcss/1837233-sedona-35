72c94b2c8d5b7b5acf35c9ee577def213b94f066
pbanswer <pbanswer@yandex.ru>
2022-11-15 22:55:04 +0300

    правки по замечаниям

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,17 @@
 const searchButton = document.querySelector('.navigation-item.search-hotels')
+const openModalBtn = document.querySelector('.info-block__hotel-seach-button')
 const modal = document.querySelector('.modal-container')
 const closeModalBtn = document.querySelector('.modal-content__close-btn')
 
 searchButton.addEventListener('click', () => {
     modal.classList.add('open');
 })
+
+if (openModalBtn) {
+    openModalBtn.addEventListener('click', () => {
+        modal.classList.add('open');
+    })
+}
 
 modal.addEventListener('click', (evt) => {
     if (evt.target.classList.contains('modal-container')) {
